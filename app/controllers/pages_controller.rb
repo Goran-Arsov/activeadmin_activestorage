0eb65bd9d7bb224a271d192a10a76ce1f71ade68
Goran-Arsov <arsphy@yahoo.com>
2019-10-15 12:02:07 +0200

    Add user-page association

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,7 +26,7 @@ class PagesController < ApplicationController
   # POST /pages.json
   def create
     @page = Page.new(page_params)
-
+    @page.user = current_user
     respond_to do |format|
       if @page.save
         format.html { redirect_to @page, notice: 'Page was successfully created.' }
@@ -41,6 +41,7 @@ class PagesController < ApplicationController
   # PATCH/PUT /pages/1
   # PATCH/PUT /pages/1.json
   def update
+    @user.id = current_user.id
     respond_to do |format|
       if @page.update(page_params)
         format.html { redirect_to @page, notice: 'Page was successfully updated.' }
@@ -70,6 +71,6 @@ class PagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
-      params.require(:page).permit(:title, :body)
+      params.require(:page).permit(:title, :body, :user_id)
     end
 end
